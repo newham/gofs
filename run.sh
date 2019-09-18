0006a1854c557138a3bd86abf55a46c39d6b7d4e
Newham <newham.cn@gmail.com>
2019-09-18 14:27:05 +0800

    modify run.shq!

--- run.sh
+++ run.sh
@@ -1,1 +1,31 @@
-./gofs
+#!/bin/bash
+
+f=gofs.service
+name=gofs
+exc=gofs
+
+sudo rm ./$exc
+
+wget https://github.com/newham/gofs/releases/download/v1.0/$exc
+
+sudo chmod +x $exc
+
+echo "[Unit]
+Description=$name
+
+[Service]
+WorkingDirectory=$(pwd)
+ExecStart=$(pwd)/$exc
+User=$(whoami)
+
+[Install]
+WantedBy=multi-user.target
+" > $f
+
+sudo mv $f /lib/systemd/system
+
+sudo systemctl enable $f
+
+sudo systemctl restart $f
+
+sudo systemctl status $f
